9641de2a7826259a5cf96a0278af70054797d5fe
edsnasc <edsnascimento89@gmail.com>
2021-01-08 01:17:34 -0300

    initial commit

---
+++ movie-search/src/components/Search.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="search" >
+      <form action="#">
+          <input type="text" v-model="searchmovie" placeholder="Pesquisar Filme"><button type="submit">Pesquisar</button>
+      </form>    
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { baseApiUrl, ApiKey } from '@/global'
+
+export default {
+    name: 'Search',
+    data: function(){
+        return {
+            searchmovie: "Batman"
+
+        }
+    },
+    methods: {
+        getMovie() {
+            const id = this.searchmovie
+            const url = `${baseApiUrl}${id}${ApiKey}`
+            axios.get(url).then(res => {
+                console.log(res)
+            })
+        }
+    },
+    mounted() {
+        this.getMovie()
+    }   
+}
+</script>
+
+<style>
+    .search {
+        display: flex; 
+        justify-content: center;
+        align-items: center;
+    }
+
+    
+</style>
---
+++ movie-search/src/global.js
@@ -0,0 +1,4 @@
+export const ApiKey = '&apikey=bde76fc4'
+export const baseApiUrl = 'http://www.omdbapi.com/?t='
+
+export default {ApiKey, baseApiUrl}
