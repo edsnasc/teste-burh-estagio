4c708e584c95028d6402e8246eb26a5eab715679
edsnasc <edsnascimento89@gmail.com>
2021-01-10 01:38:51 -0300

    assigning values

--- movie-search/src/App.vue
+++ movie-search/src/App.vue
@@ -9,17 +9,20 @@ import Search from './components/Search'
 
 export default {
   name: 'App',
-  components: { Search },
+  components: { Search }
 }
 </script>
 
 <style>
 * {
   margin: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;  
 }
 
 body {
   background: linear-gradient(to right, rgb(0, 0, 0), rgb(67, 67, 67));
+  color: #fff;
 }
 
 </style>
--- movie-search/src/components/Search.vue
+++ movie-search/src/components/Search.vue
@@ -1,57 +1,130 @@
 <template>
-<div class="search">
-  <img class="logo" src="@/assets/logo.png" alt="logo" width="250" height="200">
-  <h1>Search Movies</h1>
-  <div >
-      <input type="text" v-model="searchKey" placeholder="Pesquisar Filme">
-      <button @click="getMovie">Pesquisar</button>
+  <div>
+    <header class="header-container">
+      <img
+        class="logo"
+        src="@/assets/logo.png"
+        alt="logo"
+        width="250"
+        height="200"
+      />
+      <h1>Search Movies & Series</h1>
+    </header>
+    <div class="search-container">
+      <input type="text" v-model="searchKey" placeholder="Search movie" />
+      <button @click="getMovie">Search</button>
+    </div>
+    <div class="information-container" v-if="isContentVisible">
+    <div class="information">
+      <img :src="movie.Poster" alt="poster" />
+      <ul>
+        <li><strong>Title: </strong> {{ movie.Title }}</li>
+        <li><strong>Year: </strong> {{ movie.Year }}</li>
+        <li><strong>Runtime: </strong> {{ movie.Runtime }}</li>
+        <li><strong>Genre: </strong> {{ movie.Genre }}</li>
+        <li><strong>Director: </strong> {{ movie.Director }}</li>
+        <li><strong>Writer: </strong> {{ movie.Writer }}</li>
+        <li><strong>Actors: </strong> {{ movie.Actors }}</li>
+        <li><strong>Country: </strong> {{ movie.Country }}</li>
+        <li><strong>Production: </strong> {{ movie.Production }}</li>
+        <li><strong>Plot: </strong> {{ movie.Plot }}</li>
+      </ul>
+    </div>
   </div>
-</div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-import { baseApiUrl, ApiKey } from '@/global'
+import axios from "axios";
+import { baseApiUrl, ApiKey } from "@/global";
 
 export default {
-    name: 'Search',
-    data: function() {
-        return {
-            searchKey:'',
-            movie:[]
-
-        }
+  name: 'Search',
+  data: function () {
+    return {
+      searchKey: "",
+      movie: [],
+      isContentVisible: false
+    };
+  },
+  methods: {
+    getMovie() {
+      this.isContentVisible = true
+      const id = this.searchKey;
+      const url = `${baseApiUrl}${id}${ApiKey}`;
+      axios.get(url).then((res) => {
+        this.movie = res.data;
+      });
     },
-    methods: {
-        getMovie() {
-            const id = this.searchKey
-            const url = `${baseApiUrl}${id}${ApiKey}`
-            axios.get(url).then(res => {
-                this.movie = res
-            })
-
-        }
-    }
+  },
 }
 </script>
 
 <style>
-   
-    .search {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;  
-    }
+.header-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+}
 
-    .search input {
-        width: 700px;
-        height: 20px;
-        outline: none; 
-    }
+.search-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    .search button {
-        height: 25px;
-    }
-    
+  margin-bottom: 20px;
+}
+
+.search-container input {
+  width: 700px;
+  height: 25px;
+  outline: none;
+}
+
+.search-container button {
+  height: 25px;
+  border: none;
+  color: #fff;
+  background-color: red;
+  box-shadow: 0 0 20px black;
+  outline: none;
+}
+
+.search-container button:hover {
+   background-color:#E57373;
+}
+
+.information-container {
+  display: flex;
+  justify-content: center;
+}
+
+.information {
+  width: 1000px;
+  height: 400px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 0 20px black;
+  background: linear-gradient(
+    to right,
+    rgb(15, 32, 39),
+    rgb(32, 58, 67),
+    rgb(44, 83, 100)
+  );
+}
+
+.information ul {
+  list-style: none;
+  padding-left: 10px;
+}
+.information img {
+  position: relative;
+  width: 260px;
+  height: 350px;
+  padding: 10px 0px 10px 10px;
+}
+
 </style>
